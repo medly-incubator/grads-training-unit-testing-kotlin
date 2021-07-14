c7c84a8acf9d25bad44890a1a08b2d18a47cb033
Abhinav Manchanda <abhinav@abhinav.local>
2021-07-14 18:57:13 +0530

    Calculate prime factors of 6

--- src/test/kotlin/com/example/unittesting/PrimeFactorsCalculatorTest.kt
+++ src/test/kotlin/com/example/unittesting/PrimeFactorsCalculatorTest.kt
@@ -6,23 +6,23 @@ import io.kotest.matchers.shouldBe
 class PrimeFactorsCalculatorTest : StringSpec({
     val primeFactorsCalculator = PrimeFactorsCalculator()
     "prime factors of 1 should be empty list" {
-        val emptyList = emptyList<Int>()
-        primeFactorsCalculator.factorsOf(1) shouldBe emptyList
+        primeFactorsCalculator.factorsOf(1) shouldBe emptyList()
     }
 
     "prime factors of 2 should be 2" {
-        val expected = listOf(2)
-        primeFactorsCalculator.factorsOf(2) shouldBe expected
+        primeFactorsCalculator.factorsOf(2) shouldBe listOf(2)
     }
 
     "prime factors of 3 should be 3" {
-        val expected = listOf(3)
-        primeFactorsCalculator.factorsOf(3) shouldBe expected
+        primeFactorsCalculator.factorsOf(3) shouldBe listOf(3)
     }
 
     "prime factors of 4 should be 2,2" {
-        val expected = listOf(2,2)
-        primeFactorsCalculator.factorsOf(4) shouldBe expected
+        primeFactorsCalculator.factorsOf(4) shouldBe listOf(2, 2)
+    }
+
+    "prime factors of 6 should be 2,3" {
+        primeFactorsCalculator.factorsOf(6) shouldBe listOf(2,3)
     }
 
 })
